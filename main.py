69e56c2bd72a3f5e216d49a8865795c3a875cb9b
Ojaswee19 <104317733+Ojaswee19@users.noreply.github.com>
2023-11-05 20:45:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Kaal ki kayanat me welcome")
+# print("Kaal ki kayanat me welcome")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
